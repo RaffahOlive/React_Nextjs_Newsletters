03406f82097d97bac2a9e159fdd014c671c47263
Rafael <rafaelribeiro12@live.com>
2022-03-23 10:16:07 -0300

    created main content layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-
 import Head from 'next/head'
+
+import styles from './home.module.scss';
 
 export default function Home()
 {
@@ -8,7 +9,17 @@ export default function Home()
       <Head>
         <title>ig.news</title>
       </Head>
-      <h1>Hello World</h1>
+      <main className={ styles.contentContainer }>
+        <section className={ styles.hero }>
+          <span> 👋 Hey, hwelcome</span>
+          <h1>news about the <span>React</span> world </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for R$9,90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
 
   )
