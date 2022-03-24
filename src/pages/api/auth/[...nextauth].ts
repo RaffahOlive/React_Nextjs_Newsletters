611b7a94e7701ad8cd07d1f517d70c065a040a09
Rafael <rafaelribeiro12@live.com>
2022-03-24 19:51:04 -0300

    faunaDB configured

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb"
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from "../../../services/fauna"
 
 
 export default NextAuth({
@@ -14,4 +18,23 @@ export default NextAuth({
             }
         }),
     ],
+    callbacks: {
+        async signIn({ user, account, profile })
+        {
+            const { email } = user
+
+            try
+            {
+                await fauna.query(
+                    q.Create(
+                        q.Collection('user'),
+                        { data: { email } }
+                    )
+                )
+                return true
+            } catch {
+                return false
+            }
+        }
+    }
 })
